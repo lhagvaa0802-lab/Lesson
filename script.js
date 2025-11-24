f487d078c54fabc2f4c4b9be2d0aeafce67b6ecc
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-11-24 12:30:34 +0800

    first

---
+++ script.js
@@ -0,0 +1,60 @@
+// for (i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// for (i = 1; i <= 10; i++) {
+//   let n = 3;
+//   n = n * i;
+//   console.log(n);
+// }
+// const a = 2;
+// let n = 5;
+// let b;
+// for (i = 1; i <= n; i++) {
+//   b = a ** i;
+// }
+// console.log(b);
+
+// n = 123456789;
+// let sum = 0;
+// const length = String(n).length;
+// console.log(String(n).length, "urt");
+
+// for (let i = 1; i <= length; i++) {
+//   if (n >= 10 ** (length - i)) {
+//     let firstDigit = Math.floor(n / 10 ** (length - i));
+//     n = n - firstDigit * 10 ** (length - i);
+//     sum += firstDigit;
+//   }
+// }
+
+// let n = 1236438999999;
+// for (count = 0; n >= 1; count++) {
+//   n = Math.floor(n / 10);
+// }
+// console.log(count);
+
+// function mySum(a, b) {
+//   return a + b;
+// }
+
+// result = mySum(5, 1);
+// console.log(result);
+
+// function mySum(a) {
+//   return a * a;
+// }
+
+// result = mySum(3);
+// console.log(result);
+
+// function myNumber(b) {
+//   let a = b % 2;
+//   if (a === 0) {
+//     console.log("Tegsh");
+//   } else {
+//     console.log("sondgoi");
+//   }
+// }
+
+// myNumber(3);
