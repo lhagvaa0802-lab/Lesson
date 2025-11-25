e2f9660e0072c9f8620fe25017ed1a2e06fc9720
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-11-25 12:15:49 +0800

    chages

--- script.js
+++ script.js
@@ -58,3 +58,94 @@
 // }
 
 // myNumber(3);
+
+// function great(name) {
+//   return console.log("Hello Lhagvaa");
+// }
+
+// great();
+
+// function mySum(a, b) {
+//   return a + b;
+// }
+
+// console.log(mySum(3, 1));
+
+// function myMAX(a, b) {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+// console.log(myMAX(5, 4));
+
+// const friut = ["ggg", "hhhh"];
+// function myArray(arr) {
+//   return arr.length;
+// }
+
+// console.log(myArray(friut));
+
+// function arraySum(arr) {
+//   let sumNumber = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sumNumber = sumNumber + arr[i];
+//   }
+//   return sumNumber;
+// }
+
+// console.log(arraySum([1, 2, 3, 4]));
+
+// function arraySum(arr) {
+//   let reservedString = "";
+
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     const char = arr[i];
+
+//     reservedString += char;
+//   }
+//   return reservedString;
+// }
+
+// console.log(arraySum("Hello"));
+
+// let arr = "Hello";
+
+// let reservedString = " ";
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   const char = arr[i];
+
+//   reservedString += char;
+// }
+
+// console.log(reservedString);
+
+function myMax(arr) {
+  let maxNumber = arr[0];
+  for (i = 1; i < arr.length; i++) {
+    if (maxNumber < arr[i]) {
+      maxNumber = arr[i];
+    }
+  }
+  return maxNumber;
+}
+
+let number = [44, 7, 3, 4, 10, 12];
+console.log(myMax(number));
+
+// function myfunction(str) {
+//   let char = "";
+//   for (i = 0; i < str.length; i++) {
+//     char = char + str[i];
+//     if (i === 0) {
+//       char = char.toUpperCase();
+//     }
+//   }
+//   return char;
+// }
+// let myWord = "rdsd";
+// console.log(myfunction(myWord));
